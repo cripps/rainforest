2941bcb8dbf6aaf09fdb151d4d012f7e1cf6ab0a
cripps <rob.cripps@gmail.com>
2015-04-20 16:23:58 -0400

    Displaying price in dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
+
+  def formatted_price
+    new_price = price_in_cents.to_f / 100
+    sprintf("%.2f", new_price)
+  end
 end

5728e872eda28a746e31b10c747cd94279e7ba9d
cripps <rob.cripps@gmail.com>
2015-04-23 17:11:03 -0400

    Writing the review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   root 'products#index'
-  resources :products
+
   resources :users, only:[:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+review = Review.create([{comment: "YUM YUM", product_id: 4}])
